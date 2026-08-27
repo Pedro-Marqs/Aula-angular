b22df6d480a2a4d0d136f73947913eb75a9b715d
Pedro <pedrompro01@gmail.com>
2026-08-27 20:50:00 -0300

    html list

--- src/app/features/product/list/list.ts
+++ src/app/features/product/list/list.ts
@@ -1,14 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../../../commons/services/product-service';
 import { ProductInterface } from '../interfaces/product.interface';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List implements OnInit {
+  [x: string]: any;
   public products: ProductInterface[] = [];
 
   private productService = inject(ProductService);
